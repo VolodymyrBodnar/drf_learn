d090f409125279049aa0eb6d44ef007ffc0092c0
Volodymyr Bodnar <volodymyr.bodnar@vakoms.com>
2019-07-09 10:33:41 +0300

    add password to user registration

--- drf/users/serializers.py
+++ drf/users/serializers.py
@@ -4,6 +4,20 @@ from . import models
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        user = models.User.objects.create(
+            email=validated_data.get('email'),
+            first_name=validated_data.get('first_name'),
+            last_name=validated_data.get('last_name'),
+            birth_date=validated_data.get('birth_date')
+        )
+
+        user.set_password(validated_data.get('password'))
+        user.save()
+
+        return user
+
     class Meta:
         model = models.User
-        fields = ('email', 'first_name', 'last_name', 'birth_date')
+        fields = ('email', 'password', 'first_name', 'last_name', 'birth_date')
